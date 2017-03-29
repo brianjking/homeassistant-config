6cdf51a642ab507bc0b760c202228a7d935cb76d
Brian King <brianjosephking@gmail.com>
2017-03-28 22:27:37 -0500

    Adding heaps of scripts

---
+++ bin/update_config_from_github.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd "/home/homeassistant/.homeassistant/"
+git pull
---
+++ bin/update_new_commits_sensor.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd "/home/homeassistant/.homeassistant"
+git fetch
+commits="$(git rev-list --count master..origin/master)"
+
+curl -X POST -H "x-ha-access: $1" -H "Content-Type: application/json" https://raptorbot.ddns.net/api/states/sensor.new_commits -d "{\"state\": \"$commits\"}"
