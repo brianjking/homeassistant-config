0044ee5942e1a26d8fb26f4de4a476189cabf8d1
Brian King <brianjosephking@gmail.com>
2017-03-29 00:34:28 -0500

    debug

--- bin/update_new_commits_sensor.sh
+++ bin/update_new_commits_sensor.sh
@@ -4,4 +4,4 @@ cd "/home/homeassistant/.homeassistant"
 git fetch
 commits="$(git rev-list --count master..origin/master)"
 
-curl -X POST -H "x-ha-access: $1" -H "Content-Type: application/json" https://localhost:8123/api/states/sensor.new_commits -d "{\"state\": \"$commits\"}"
+curl -X POST -H "x-ha-access: $1" -H "Content-Type: application/json" http://localhost:8123/api/states/sensor.new_commits -d "{\"state\": \"$commits\"}"
