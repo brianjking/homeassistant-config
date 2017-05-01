ca45c7669c2e1242497eb4f69318f0e419f9f21e
Brian King <brianjosephking@gmail.com>
2017-05-01 17:38:38 -0500

    Oops

--- bin/update_new_commits_sensor.sh
+++ bin/update_new_commits_sensor.sh
@@ -4,4 +4,4 @@ cd "/home/homeassistant/.homeassistant"
 git fetch
 commits="$(git rev-list --count master..origin/master)"
 
-curl -X POST -H "x-ha-access: $1" -H "Content-Type: application/json" https://raptorbot.ddns.net/api/states/sensor.new_commits?api_password=DeadParrot! -d "{\"state\": \"$commits\"}"
+curl -X POST -H "x-ha-access: $1" -H "Content-Type: application/json" http://127.0.0.1:8123/api/states/sensor.new_commits -d "{\"state\": \"$commits\"}"
